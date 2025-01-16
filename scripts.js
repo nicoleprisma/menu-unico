a3dd29f0463fbc36387baf882c8209001b441504
n.soares <n.soares@riprisma.com>
2025-01-16 10:56:30 -0300

    primeiro commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+// ativa menu mobile
+const abrirMenu = document.querySelector(".open-menu"),
+  menu = document.querySelector(".menu");
+
+function MenuMobile() {
+  menu.classList.toggle("mobile-active");
+}
+
+abrirMenu.addEventListener("click", MenuMobile);
+
+//ativa dropdown
+const dropdownItem = document.querySelectorAll(".dropdown");
+
+dropdownItem.forEach((item) => {
+  const dropdownMenu = item.querySelector(".dropdown-item");
+
+  item.addEventListener("mouseenter", () => {
+    dropdownMenu.classList.toggle("active");
+  });
+
+  item.addEventListener("mouseleave", () => {
+    dropdownMenu.classList.toggle("active");
+  });
+});
