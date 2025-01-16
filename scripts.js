a334e665492df4a3c7ec05ba244dfe0aa7cf208a
n.soares <n.soares@riprisma.com>
2025-01-16 11:18:32 -0300

    teste no dropdown mobile v3

--- scripts.js
+++ scripts.js
@@ -9,26 +9,26 @@ function MenuMobile() {
 abrirMenu.addEventListener("click", MenuMobile);
 
 //ativa dropdown
-const dropdownItem = document.querySelectorAll(".dropdown");
+// const dropdownItem = document.querySelectorAll(".dropdown");
 
-dropdownItem.forEach((item) => {
-  const dropdownMenu = item.querySelector(".dropdown-item");
+// dropdownItem.forEach((item) => {
+//   const dropdownMenu = item.querySelector(".dropdown-item");
 
-  item.addEventListener("mouseenter", () => {
-    dropdownMenu.classList.toggle("active");
-  });
+//   item.addEventListener("mouseenter", () => {
+//     dropdownMenu.classList.toggle("active");
+//   });
 
-  item.addEventListener("mouseleave", () => {
-    dropdownMenu.classList.toggle("active");
-  });
+//   item.addEventListener("mouseleave", () => {
+//     dropdownMenu.classList.toggle("active");
+//   });
 
-  if (window.matchMedia("(max-width: 991px)").matches) {
-    item.addEventListener("click", () => {
-      dropdownMenu.classList.toggle("active");
-    });
+//   if (window.matchMedia("(max-width: 991px)").matches) {
+//     item.addEventListener("click", () => {
+//       dropdownMenu.classList.toggle("active");
+//     });
 
-    item.addEventListener("click", () => {
-      dropdownMenu.classList.toggle("active");
-    });
-  }
-});
+//     item.addEventListener("click", () => {
+//       dropdownMenu.classList.toggle("active");
+//     });
+//   }
+// });
